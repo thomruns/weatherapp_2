93669e51c346b7212ef71b06e224a51f355b679a
Thom David <thomdavid74@gmail.com>
2019-04-19 15:40:07 -0700

    Use ternary operator for image src

--- scripts/app.js
+++ scripts/app.js
@@ -42,13 +42,10 @@ const updateUI = (data) => {
     // update the night/day icon images
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
-    
-    let timeSrc = null;
-    if(weather.IsDayTime) {
-      timeSrc = 'img/day.svg';
-    } else {
-      timeSrc = 'img/night.svg';
-    }
+
+    // check to see if it is day or night
+    let timeSrc = (weather.IsDayTime ? 'img/day.svg' : 'img/night.svg');
+
     time.setAttribute('src', timeSrc);
 
     // remove d-none class if present
