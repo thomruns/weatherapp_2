62574250e312bf130cfcaa8ec858c51431251e52
Thom David <thomdavid74@gmail.com>
2019-04-19 14:44:45 -0700

    Add API data to UI

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // script for DOM manipulation
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
 const updateCity = async (city) => {
   // use the location API function in forecast.js
@@ -14,6 +16,25 @@ const updateCity = async (city) => {
   }
 }
 
+const updateUI = (data) => {
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+        <span>${weather.Temperature.Imperial.Value}</span>
+        <span>&deg;F</span>
+      </div>
+    `;
+    // remove d-none class if present
+    if(card.classList.contains('d-none')) {
+      card.classList.remove('d-none');
+    }
+}
+
 // event listener on UI form input field
 cityForm.addEventListener('submit', e => {
   // prevent default action
@@ -25,6 +46,6 @@ cityForm.addEventListener('submit', e => {
   // update UI w new city, async calls to API
   // returns a promise
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
