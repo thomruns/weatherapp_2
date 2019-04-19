aaff4596e4e3233aeb28549970fe1c1d05e1fdfb
Thom David <thomdavid74@gmail.com>
2019-04-19 10:13:29 -0700

    fetch city data from API

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WeatherApp 2
 
 A simple weather app using the AccuWeather API
+
+The AccuWeather API in use here requires an API key. Two API GET requests are required: first to the locations API to get the key code of the requested city, then, using that key code, a second request to get the current conditions for that location.
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,3 +2,24 @@
 
 // AccuWeather API Key here:
 const key = 'API_KEY_HERE';
+
+const getCity = async (city) => {
+  
+  // base URL of API location endpoint
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  // complete the query string required to get location
+  const query = `?apikey=${key}&q=${city}`;
+
+  // use fetch w the constructed query string
+  const response = await fetch(base + query); // returns a promise
+  
+  // process response
+  const data = await response.json();
+
+  // 
+  return data[0];
+}
+
+getCity('Oceanside')
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
